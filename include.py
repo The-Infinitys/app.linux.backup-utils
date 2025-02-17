ba028cfae32ced638a40887120828dbebdaa7fe0
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-02-17 01:17:24 +0000

    asdf

--- include.py
+++ include.py
@@ -2,7 +2,7 @@ import os, sys
 include_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "./library"))
 sys.path.append(include_dir)
 sys.path.append(include_dir+"/textual")
-from library import manage, git_utils, textual, textual_utils
+from library import manage, git_utils, textual, textual_utils, terminal
 from library.textual.textual.binding import Binding
 from library.textual.textual.app import App, ComposeResult
 from library.textual.textual.color import Gradient, Color
